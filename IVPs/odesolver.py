634ecd4ff57cf95d26a59b2158b72ddc4da2a34c
edatkinson <98823225+edatkinson@users.noreply.github.com>
2024-01-29 11:31:14 +0000

    added rk4 step

--- IVPs/odesolver.py
+++ IVPs/odesolver.py
@@ -24,6 +24,14 @@ def euler_step(f,x,t,h):
     x = x+ h*f(t,x)
     return x
 
+def rk4_step(f,x,t,h):
+    k1 = f(t,x)
+    k2 = f(t+h/2, x+h*(k1/2))
+    k3 = f(t+h/2,x+h*(k2/2))
+    k4 = f(t+h,x+h*k3)
+    x = x + h/6 * (k1 + 2*k2 + 2*k3 + k4)
+    return x
+
 def dx_dt(t,x):
     dx_dt = x
     return dx_dt
@@ -38,10 +46,21 @@ h = 0.1
 t = t0
 x = x0
 
+t_list = []
+x_list = []
 while t < tf:
-    x = euler_step(dx_dt, x, t, h)
+    x = rk4_step(dx_dt, x, t, h)
     t+=h
+    t_list.append(t)
     
+    x_list.append(x)
+
+plt.plot(t_list,x_list)
+plt.show()
+
+
+
+
 
 
 
