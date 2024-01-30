c738d4c2b50cfb3c326c167452b33d4d552ece29
edatkinson <98823225+edatkinson@users.noreply.github.com>
2024-01-30 15:00:06 +0000

    finding roots using fsolve

--- BVP/Week15.py
+++ BVP/Week15.py
@@ -2,11 +2,20 @@
 import numpy as np  
 import matplotlib.pyplot as plt 
 from scipy.integrate import odeint
+from scipy.optimize import fsolve
 
 #to Isolate a periodic orbit, 
+'''
+To find limit cycles, we must solve the periodic boundary value problem (BVP)
+Solve u(0) - u(T) = 0, = u0 - F(u0, T) = 0.
 
+T = 2pi/omega
 
-def lokta_volterra(x,t,beta):
+Hence, limit cycles of (3) can be found by passing (6) along with a suitable initial guess u ̃ 0 to a numerical root finder such as fsolve in Matlab or Python (Scipy) or nlsolve in Julia.
+All of the above can be trivially generalised to arbitrary periodically- forced ODEs of any number of dimensions.
+
+'''
+def lokta_volterra(x,beta,t=None):
     alpha = 1
     delta = 0.1
     dxdt = x[0]*(1-x[0]) - (alpha*x[0]*x[1])/(delta + x[0])
@@ -15,10 +24,14 @@ def lokta_volterra(x,t,beta):
 
 
 beta = 0.1  # or any other value beta is between 0.1 and 0.5
-t = np.linspace(0, 200, 500)  # time grid
+t = np.linspace(0, 100, 500)  # time grid
 x0 = [0.1, 0.1]  # initial conditions
 
 sol = odeint(lokta_volterra, x0, t, args=(beta,)) #using the odeint from scipy lib
+
+#Find the roots
+roots = fsolve(lokta_volterra, x0, args=(beta,))
+print('Roots:', roots)
 
 plt.plot(t, sol[:, 0], label='Prey')
 plt.plot(t, sol[:, 1], label='Predator')
