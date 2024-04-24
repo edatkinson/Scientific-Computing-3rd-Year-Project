5a76a18da19dfb76d59d3d653581aef34b43c871
edatkinson <98823225+edatkinson@users.noreply.github.com>
2024-04-24 18:03:39 +0100

    Added APIs to each function

--- Work/bvp_and_shooting.py
+++ Work/bvp_and_shooting.py
@@ -16,24 +16,48 @@ warnings.filterwarnings('ignore', category=OptimizeWarning)
 #####Root finding problem and shooting########
 
 def phase_condition(ode,u0,pars):
-    #return the phase condition which is du1/dt(0) = 0
+    """
+    Calculate the phase condition for a system of ordinary differential equations (ODEs).
+    
+    This function evaluates the phase condition, which is the derivative of the first state variable
+    with respect to time at t = 0, and it should be zero to satisfy the phase condition.
+
+    Args:
+        ode (callable): A function representing the system of ODEs. It must accept three parameters:
+                        time (scalar), state vector (array), and parameters (tuple).
+        u0 (array): Initial condition of the system.
+        pars (tuple or list): Parameters required for the ODE function.
+
+    Returns:
+        array: The evaluated phase condition, specifically the value of the first derivative of the
+               first state variable at t = 0.
+    """
     return np.array([ode(0,u0,pars)[0]])
 
 def shoot(f, phase_cond):
+    """
+    Generate a boundary-value problem solver using the shooting method.
+    
+    This function creates a solver for finding periodic solutions in a system of ODEs by adjusting
+    the initial conditions to match the final conditions after one period.
+
+    Args:
+        f (callable): The function representing the system of ODEs.
+        phase_cond (callable): The function to evaluate the phase condition.
+
+    Returns:
+        callable: A function G that accepts initial conditions, period T, and parameters, and
+                  calculates the discrepancy between the desired and actual final states, including
+                  the phase condition.
+    """
     def G(u0, T, pars):
- 
-        # Solve ODE system using Solve IVP 
+
         t = np.linspace(0, T, 1000)
         try:
-            # max_step = 0.1
-            # sol = solve_ivp(f, (0, T), u0,args=(pars,), method='RK45',max_step=max_step)
-            # final_sol = sol.y[:,-1]
-            
-            t = np.linspace(0, T, 1000)
             sol = solve_ode(f, u0, t, "rk4", 0.01, pars) #solve the the ode from 0 to T 
             final_sol = sol[:, -1] #Excracr the final solution
             if np.isnan(sol).any():
-                raise ValueError("The ODE solver returned NaN values, which indicates a problem with the ODE integration.") #Check for NaN values and return error if found
+                raise ValueError("The ODE solver returned NaN values, which indicates a problem with the ODE integration. Ensure you are using the correct Initial Conditions.") #Check for NaN values and return error if found
             if phase_cond == None:
                 return 0
             else:
@@ -45,16 +69,41 @@ def shoot(f, phase_cond):
 
 
 def orbit(ode, uinitial, duration,pars):
+    """
+    Solve an ODE system over a specified duration to generate an orbit.
+    
+    Args:
+        ode (callable): The system of ODEs to be solved.
+        uinitial (array): Initial conditions for the ODE solver.
+        duration (float): Time interval over which to solve the ODE.
+        pars (tuple): Parameters required for the ODE function.
 
-
+    Returns:
+        tuple: A tuple containing the solution array and the time array.
+    """
     t = np.linspace(0,duration,150)
     sol = solve_ivp(ode, (0, duration), uinitial,t_eval=t ,args=(pars,))
     
     return sol.y, t
 
 def limit_cycle_finder(ode, estimate, phase_condition, pars,descretization=shoot, solver=fsolve, test=False):
-   
+    """
+    Find a limit cycle for a system of ODEs using a specified root-finding method and shooting method.
 
+    Args:
+        ode (callable): The system of ODEs.
+        estimate (array): Initial guess for the root-finder, including initial conditions and period.
+        phase_condition (callable): Function to evaluate the phase condition.
+        pars (tuple): Parameters for the ODEs.
+        descretization (callable, optional): Function to convert the boundary value problem to an
+                                             initial value problem via the shooting method.
+        solver (callable, optional): Root-finding method to solve the initial value problem.
+        test (bool, optional): If True, print additional diagnostics about the solver.
+
+    Returns:
+        array: The solution array containing the final initial conditions and period that approximately
+               satisfy the boundary conditions, forming a limit cycle.
+    """
     G = descretization(ode,phase_condition)
     try:
         solution, info, ier, msg = solver(lambda estimate: G(estimate[:-1], estimate[-1], pars), estimate, full_output=True)
@@ -70,25 +119,6 @@ def limit_cycle_finder(ode, estimate, phase_condition, pars,descretization=shoot
         return solution 
     except Exception as e:
         raise RuntimeError(f"An unexpected error occurred: {e}")
-
-
-
-def phase_portrait_plotter(sol):
-    plt.plot(sol[0,:], sol[1,:], label='Isolated periodic orbit')
-    plt.xlabel('$x$')
-    plt.ylabel('$y$')
-    plt.title('Phase portrait')
-    plt.legend()
-
-    #Code to plot the 3D Hopf limit cycle:
-    # fig = plt.figure()
-    # ax = fig.add_subplot(111, projection='3d')
-    # ax.plot(sol[:,0], sol[:,1], sol[:,2], label='Isolated periodic orbit')
-    # ax.set_xlabel('u1')
-    # ax.set_ylabel('u2')
-    # ax.set_zlabel('u3')
-    
-    return plt#, fig
 
 
 #Create a Main function which does the code below
